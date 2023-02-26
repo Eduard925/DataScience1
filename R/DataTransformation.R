866dbd10de9eb31de0a4957143be0382691ec15e
Eduard925 <eduardd.romeroa@ecci.edu.co>
2023-02-26 18:21:05 -0500

    Commit

--- R/DataTransformation.R
+++ R/DataTransformation.R
@@ -4,3 +4,16 @@ author: "Eduard Romero"
 date: "2023-02-25"
 output: pdf_document
 ---
+
+# Configuramos Chuck Code and incluid the librarys.
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+library(dplyr)
+library(nycflights13)
+library(tidyverse)
+
+```
+# look at flights data
+```{r}
+flights
+```
