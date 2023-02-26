37f809d2be190601769379ef1264f1588e931e64
Eduard925 <eduardd.romeroa@ecci.edu.co>
2023-02-26 18:14:27 -0500

    Test

---
+++ R/DataTransformation.R
@@ -0,0 +1,6 @@
+---
+  title: "Supervised Methods in Machine Learning report guideline: Exploration and Application of R for Data Science"
+author: "Eduard Romero"
+date: "2023-02-25"
+output: pdf_document
+---
